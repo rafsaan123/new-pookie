707ae6fd77b9a5c79783e0101262d17db6486953
rafsaan123 <79746855+rafsaan123@users.noreply.github.com>
2025-10-02 14:17:09 +0600

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/_next/', '/admin/'],
     },
-    sitemap: 'https://bteb-results.vercel.app/sitemap.xml',
+    sitemap: 'https://btebresultshub.com/sitemap.xml',
   }
 }

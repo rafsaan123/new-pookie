fc938e0ad5011c10d2d846fd62b7d9329fd9cf8a
rafsaan123 <rafsaan123@users.noreply.github.com>
2025-10-13 00:00:17 +0600

    Move Ad 1 to appear right after search tool on main page
    - Position Ad 1 immediately after search form
    - Place it before error messages and results
    - Ensures ad appears right after the tool as requested
    - Maintains clean user experience

--- app/page.tsx
+++ app/page.tsx
@@ -203,6 +203,28 @@ export default function HomePage() {
             </button>
           </form>
 
+          {/* Ad 1 - After search form */}
+          <div className="mt-8 mb-8">
+            <div className="bg-gray-50 rounded-lg p-4 text-center">
+              <div id="ad-1" className="min-h-[90px] flex items-center justify-center">
+                <div dangerouslySetInnerHTML={{
+                  __html: `
+                    <script type="text/javascript">
+                      atOptions = {
+                        'key' : 'f9fd39af8932f3a0a9c36fefbb67ec82',
+                        'format' : 'iframe',
+                        'height' : 90,
+                        'width' : 728,
+                        'params' : {}
+                      };
+                      document.write('<scr' + 'ipt type="text/javascript" src="https://www.highperformanceformat.com/f9fd39af8932f3a0a9c36fefbb67ec82/invoke.js"></scr' + 'ipt>');
+                    </script>
+                  `
+                }} />
+              </div>
+            </div>
+          </div>
+
           {mounted && error && (
             <div className="mt-6 bg-red-50 border border-red-200 text-red-700 rounded-lg p-4 text-sm">
               {error}
@@ -300,28 +322,6 @@ export default function HomePage() {
               </div>
             </div>
           )}
-
-          {/* Ad 1 - After search form */}
-          <div className="mt-8 mb-8">
-            <div className="bg-gray-50 rounded-lg p-4 text-center">
-              <div id="ad-1" className="min-h-[90px] flex items-center justify-center">
-                <div dangerouslySetInnerHTML={{
-                  __html: `
-                    <script type="text/javascript">
-                      atOptions = {
-                        'key' : 'f9fd39af8932f3a0a9c36fefbb67ec82',
-                        'format' : 'iframe',
-                        'height' : 90,
-                        'width' : 728,
-                        'params' : {}
-                      };
-                      document.write('<scr' + 'ipt type="text/javascript" src="https://www.highperformanceformat.com/f9fd39af8932f3a0a9c36fefbb67ec82/invoke.js"></scr' + 'ipt>');
-                    </script>
-                  `
-                }} />
-              </div>
-            </div>
-          </div>
         </div>
       </main>
 
